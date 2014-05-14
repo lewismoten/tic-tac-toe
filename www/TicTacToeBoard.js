0a1d087e6079f8d27a93ce705f30e3926ff74427
Lewis Moten <lewismoten@gmail.com>
2014-05-14 02:03:02 -0400

    Shorten logic to check for winners.

--- www/TicTacToeBoard.js
+++ www/TicTacToeBoard.js
@@ -45,33 +45,31 @@ TicTacToeBoard.prototype.changePlayer = function() {
   this.player *= -1;
   if(this.isFull()) this.player = this.empty;
   if(this.winner != this.empty) this.player = this.empty;
-}
+};
 
 TicTacToeBoard.prototype.getMark = function(column, row) {
   return this.cell[column][row];
 };
 
-TicTacToeBoard.prototype.winnerOf = function(x, y, stepX, stepY) {
+TicTacToeBoard.prototype.findWinner = function(x, y, stepX, stepY) {
   var mark = this.cell[x][y];
-  if(mark == this.empty) return this.empty;
+  if(mark == this.empty) return false;
   var i = 0;
   while(++i < 3)
   {
     var xx = x + (stepX * i);
     var yy = y + (stepY * i);
-    if(mark != this.cell[xx][yy]) return this.empty;
+    if(mark != this.cell[xx][yy]) return false;
   }
-  return mark;
+  this.winner = mark;
+  return true;
 };
 
 TicTacToeBoard.prototype.checkForWinner = function() {
   for(i = 0; i < 3; i++) {
-    this.winner = this.winnerOf(i, 0, 0, 1);
-    if(this.winner != this.empty) return;
-    this.winner = this.winnerOf(0, i, 1, 0);
-    if(this.winner != this.empty) return;
+    if(this.findWinner(i, 0, 0, 1)) return;
+    if(this.findWinner(0, i, 1, 0)) return;
   }
-  this.winner = this.winnerOf(0, 0, 1, 1);
-  if(this.winner != this.empty) return;
-  this.winner = this.winnerOf(0, 2, 1, -1);
+  if(this.findWinner(0, 0, 1, 1)) return;
+  this.findWinner(0, 2, 1, -1);
 };
