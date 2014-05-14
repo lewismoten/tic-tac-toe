7d2e81a9cf827e898321ae2cc221f96c4fdfa290
Lewis Moten <lewismoten@gmail.com>
2014-05-14 01:57:36 -0400

    Add checks for winning.

--- tests/TicTacToeBoardTests.js
+++ tests/TicTacToeBoardTests.js
@@ -8,6 +8,10 @@ describe("TicTacToeBoard", function() {
     expect(board.player).toEqual(board.player1);
   });
   
+  it("starts without a winner", function() {
+    expect(board.winner).toEqual(board.empty);
+  });
+
   it("can mark an unoccupied cell", function(){
     expect(board.mark(0, 0)).toEqual(true);
   });
@@ -37,25 +41,121 @@ describe("TicTacToeBoard", function() {
       board.mark(2, 0);
       board.mark(0, 1);
       board.mark(1, 1);
-      board.mark(2, 1);
-      board.mark(1, 2);
       board.mark(0, 2);
+      board.mark(2, 1);
       board.mark(2, 2);
+      board.mark(1, 2);
     });
     
     it("is no ones turn", function() {
       expect(board.player).toEqual(board.empty);
     });
+
+    it("has no winner", function() {
+      expect(board.winner).toEqual(board.empty);
+    });
   });
   
-  describe("win", function() {
-    it("has three X on top row", function() {
+  describe("on win", function() {
+    beforeEach(function() {
       board.mark(0, 0);
       board.mark(0, 1);
       board.mark(1, 0);
       board.mark(1, 1);
-      board.mark(0, 2);
+      board.mark(2, 0);
+    });
+    it("does not have a current player", function() {
+      expect(board.player).toEqual(board.empty);
+    });
+    it("has winner", function() {
+      expect(board.winner).not.toEqual(board.empty);
+    });
+  });
+  
+  describe("can win", function() {
+    it("by player 2", function() {
+      board.mark(0, 0);
+      board.mark(1, 0);
+      board.mark(2, 0);
+      board.mark(1, 1);
+      board.mark(2, 1);
+      board.mark(1, 2);
+      expect(board.winner).toEqual(board.player2);
+    });
+    it("by player 1", function() {
+      board.mark(0, 0);
+      board.mark(0, 1);
+      board.mark(1, 0);
+      board.mark(1, 1);
+      board.mark(2, 0);
       expect(board.winner).toEqual(board.player1);
+    });
+    it("with top row", function() {
+      board.mark(0, 0);
+      board.mark(0, 1);
+      board.mark(1, 0);
+      board.mark(1, 1);
+      board.mark(2, 0);
+      expect(board.winner).not.toEqual(board.empty);
+    });
+    
+    it("with middle row", function() {
+      board.mark(0, 1);
+      board.mark(0, 0);
+      board.mark(1, 1);
+      board.mark(1, 0);
+      board.mark(2, 1);
+      expect(board.winner).not.toEqual(board.empty);
+    });
+
+    it("with bottom row", function() {
+      board.mark(0, 2);
+      board.mark(0, 0);
+      board.mark(1, 2);
+      board.mark(1, 0);
+      board.mark(2, 2);
+      expect(board.winner).not.toEqual(board.empty);
+    });
+    
+    it("with left column", function() {
+      board.mark(0, 0);
+      board.mark(1, 0);
+      board.mark(0, 1);
+      board.mark(1, 1);
+      board.mark(0, 2);
+      expect(board.winner).not.toEqual(board.empty);
+    });
+    it("with right column", function() {
+      board.mark(2, 0);
+      board.mark(1, 0);
+      board.mark(2, 1);
+      board.mark(1, 1);
+      board.mark(2, 2);
+      expect(board.winner).not.toEqual(board.empty);
+    });
+    it("with center column", function() {
+      board.mark(1, 0);
+      board.mark(2, 0);
+      board.mark(1, 1);
+      board.mark(2, 1);
+      board.mark(1, 2);
+      expect(board.winner).not.toEqual(board.empty);
+    });
+    it("with top-right diagonal", function() {
+      board.mark(2, 0);
+      board.mark(1, 0);
+      board.mark(1, 1);
+      board.mark(0, 1);
+      board.mark(0, 2);
+      expect(board.winner).not.toEqual(board.empty);
+    });
+    it("with top-left diagonal", function() {
+      board.mark(0, 0);
+      board.mark(1, 0);
+      board.mark(1, 1);
+      board.mark(2, 1);
+      board.mark(2, 2);
+      expect(board.winner).not.toEqual(board.empty);
     });
   });
 });
--- www/TicTacToeBoard.js
+++ www/TicTacToeBoard.js
@@ -33,21 +33,45 @@ TicTacToeBoard.prototype.isFull = function() {
 };
 
 TicTacToeBoard.prototype.mark = function(column, row) {
-
-  if(this.cell[column][row] === this.empty) {
-    this.cell[column][row] = this.player;
-    this.checkForWinner();
-    this.player *= -1;
-    if(this.isFull()) this.player = this.empty;
-    return true;
-  }
-  return false;
+  if(this.player == this.empty) return false;
+  if(this.cell[column][row] != this.empty) return false;
+  this.cell[column][row] = this.player;
+  this.checkForWinner();
+  this.changePlayer();
+  return true;
 };
+
+TicTacToeBoard.prototype.changePlayer = function() {
+  this.player *= -1;
+  if(this.isFull()) this.player = this.empty;
+  if(this.winner != this.empty) this.player = this.empty;
+}
 
 TicTacToeBoard.prototype.getMark = function(column, row) {
   return this.cell[column][row];
-}
+};
+
+TicTacToeBoard.prototype.winnerOf = function(x, y, stepX, stepY) {
+  var mark = this.cell[x][y];
+  if(mark == this.empty) return this.empty;
+  var i = 0;
+  while(++i < 3)
+  {
+    var xx = x + (stepX * i);
+    var yy = y + (stepY * i);
+    if(mark != this.cell[xx][yy]) return this.empty;
+  }
+  return mark;
+};
 
 TicTacToeBoard.prototype.checkForWinner = function() {
-  this.winner = this.player1;
-}
+  for(i = 0; i < 3; i++) {
+    this.winner = this.winnerOf(i, 0, 0, 1);
+    if(this.winner != this.empty) return;
+    this.winner = this.winnerOf(0, i, 1, 0);
+    if(this.winner != this.empty) return;
+  }
+  this.winner = this.winnerOf(0, 0, 1, 1);
+  if(this.winner != this.empty) return;
+  this.winner = this.winnerOf(0, 2, 1, -1);
+};
