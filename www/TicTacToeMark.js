392decb3c5529b575d4e7c47fe823af938ef85fa
Lewis Moten <lewismoten@gmail.com>
2014-05-15 17:49:09 -0400

    Remove need to calculate text string

--- www/TicTacToeMark.js
+++ www/TicTacToeMark.js
@@ -1,8 +1,8 @@
 function TicTacToeMark(onMark) {
-  var player1 = 1;
-  var player2 = -1;
-  var empty = 0;
-  var value = empty;
+  var player1 = "X";
+  var player2 = "O";
+  var empty = " ";
+  var value = " ";
   
   this.mark = onMark;
 
@@ -19,14 +19,11 @@ function TicTacToeMark(onMark) {
   };
   
   this.data = function() {
-    var s = value == empty ? ' ' :
-      value == player1 ? 'X' :
-      value == player2 ? 'O' : '?';
     return {
       isFirstPlayer: value == player1,
       isSecondPlayer: value == player2,
       isEmpty: value == empty,
-      text: s
+      text: value
       };
   };
   
